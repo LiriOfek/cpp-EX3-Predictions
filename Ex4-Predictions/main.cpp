f94f40417ce1db1bc6f5a33612cb1894236d3bed
LiriOfek <46228594+LiriOfek@users.noreply.github.com>
2020-04-18 12:29:50 +0300

    add main file with documentation

---
+++ Ex4-Predictions/main.cpp
@@ -0,0 +1,6 @@
+/********************************************************\
+File Name: main.cpp
+Author: Liri (18/4/2020)
+Purpose: This file pass vector of numbers into predicates and print
+		 the vector of numbers that return after passing through it
+\********************************************************/
